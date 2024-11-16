d579fa0b4de7c3b75d0034ff779f5af3e233feec
FoxyMoxey <97852251+FoxyMoxey@users.noreply.github.com>
2024-11-16 10:20:22 -0800

    Update data_analysis.sql

--- data_analysis.sql
+++ data_analysis.sql
@@ -13,9 +13,9 @@ SELECT member_casual, avg(duration_in_minutes) as average_duration --Casual ride
  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.cleaned` 
  GROUP BY member_casual;
 
---Creating a view with ordered month and days
+--Creating a view with ordered month and days, extract year to its own Column
 
-CREATE VIEW `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view_fixed` as
+CREATE VIEW `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view` as
 SELECT *,
   CASE day_of_week
     WHEN 'sunday' THEN 1
@@ -40,33 +40,34 @@ SELECT *,
     WHEN 'OCT' THEN 10
     WHEN 'NOV' THEN 11
     WHEN 'DEC' THEN 12
-  END AS month_order
+  END AS month_order,
+  Extract(year from started_at) as YEAR
 
  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.cleaned` 
 
 --Member type by rides per month
-SELECT member_casual, month, count(ride_id) as total_rides,
-  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view_fixed`
-  group by month_order, month, member_casual
-  order by month_order asc;
+SELECT member_casual, month, YEAR, count(ride_id) as total_rides
+  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view`
+  group by YEAR, month_order, month, member_casual
+  order by year, month_order asc;
 
---Member type by Rides per week
+--Member type by Rides per day of weed
 SELECT member_casual, day_of_week, count(ride_id) as total_rides,
-  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view_fixed`
+  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view`
   group by day_order, day_of_week,member_casual
   order by day_order asc;
 
---Member type by rides per week, average duration and median duration
+--Member type by rides per week and month, average duration and median duration
 
-SELECT member_casual, day_of_week, count(ride_id) as total_rides, avg(duration_in_minutes) as average_duration, APPROX_QUANTILES(duration_in_minutes, 2)[OFFSET(1)] AS median_duration
-  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view_fixed`
-  group by day_order, day_of_week,member_casual
-  order by day_order asc;
+SELECT member_casual, day_of_week, month, YEAR, count(ride_id) as total_rides, avg(duration_in_minutes) as average_duration, APPROX_QUANTILES(duration_in_minutes, 2)[OFFSET(1)] AS median_duration
+  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view`
+  group by YEAR, month, day_order, day_of_week,member_casual
+  order by year, month, day_order asc;
 
 --Comparing average to median rides times to determine if data is skewed. Data appears to be skewed right. Average ride durations are higher than median. 
 
 SELECT member_casual, count(ride_id) as total_rides, avg(duration_in_minutes) as average_duration, APPROX_QUANTILES(duration_in_minutes, 2)[OFFSET(1)] AS median_duration
-  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view_fixed`
+  FROM `lively-lock-439218-m3.Bicycle_data_2022_2023.week_ordered_view`
   group by member_casual;
 
 
